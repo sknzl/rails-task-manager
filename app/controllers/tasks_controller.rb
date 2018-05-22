05dcc3cc32ebd26c86ebb8eef722740ccd7f31d9
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-22 11:04:52 +0100

    refactor, bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :fetch_task, except: [:index, :new, :create]
 
   def index
     @tasks = Task.all
@@ -14,21 +15,17 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -38,6 +35,10 @@ private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def fetch_task
+    @task = Task.find(params[:id])
   end
 
 end
